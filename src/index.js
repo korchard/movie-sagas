6fd5bfcde88834e1875ed1fd7b23d524e3c01010
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-12-19 01:00:20 -0600

    went through and commented and re-formatted all of the code

--- src/index.js
+++ src/index.js
@@ -4,62 +4,74 @@ import './index.css';
 import App from './components/App/App.js';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
+
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
+
+// Import for routes
 import { takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
 
-// Create the rootSaga generator function
+// rootSaga generator function
 function* rootSaga() {
     yield takeEvery('GET_MOVIES', getMovies);
     yield takeEvery('GET_DETAILS', getDetails);
     yield takeEvery('GET_CATEGORY', getCategory);
     yield takeEvery('ADD_MOVIE', addMovie);
-}
+} // end rootSaga
 
+// GET ROUTE - for all movies on HOME page
 function* getMovies() {
     try {
         const response = yield axios.get('/api/movie');
+        // sends data to movie reducer
         yield put({ type: 'SET_MOVIES', payload: response.data })
     } catch (error) {
-    console.log('Bad news bears, error with INDEX GET', error);
+      console.log('Bad news bears, error with INDEX GET', error);
     }
-}
+} // end getMovies
 
+// GET ROUTE - for specific movie details
 function* getDetails(action) {
     try {
         const response = yield axios.get(`/api/movie/${action.payload}`);
+        // sends specific movie to the movie reducer
         yield put({ type: 'SET_MOVIES', payload: response.data })
         console.log('getDetails', response.data);
     } catch (error) {
-    console.log('Bad news bears, error with INDEX GET', error);
+      console.log('Bad news bears, error with INDEX GET', error);
     }
-}
+} // end getDetails
 
+// GET ROUTE - for specific movie genres
 function* getCategory(action) {
     try {
         const response = yield axios.get(`/api/genre/${action.payload}`);
+        // sends specific movie genres to the genres reducer
         yield put({ type: 'SET_GENRES', payload: response.data })
         console.log('getCategory', response.data);
     } catch (error) {
-    console.log('Bad news bears, error with INDEX GET', error);
+      console.log('Bad news bears, error with INDEX GET', error);
     }
-}
+} // end getCategory
 
+// POST ROUTE
 function* addMovie( action ) {
     console.log('index post', action.payload);
     try { 
         yield axios.post('/api/movie', action.payload)
+        // calls GET ROUTE to refresh home page with new movie
         yield put({ type: 'GET_MOVIES' }) 
     } catch (error) {
         console.log('Bad news bears, error with INDEX POST', error);
     }
-}
+} // end addMovie
 
-// Create sagaMiddleware
+// sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 // Used to store movies returned from the server
@@ -67,12 +79,10 @@ const movies = (state = [], action) => {
     switch (action.type) {
         case 'SET_MOVIES':
             return action.payload;
-        // case 'SET_DETAILS':
-        //     return action.payload;
         default:
             return state;
     }
-}
+} // end movie REDUCER
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
@@ -82,7 +92,7 @@ const genres = (state = [], action) => {
         default:
             return state;
     }
-}
+} // end genre REDUCER
 
 // Create one store that all components can use
 const storeInstance = createStore(
