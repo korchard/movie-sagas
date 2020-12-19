b3862cadbc5fbd139e1f176de821d9ac86d34be6
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-12-19 10:55:00 -0600

    working on learning grid spacing with material ui

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import './MovieList.css';
 
-class Home extends Component {
+// import material UI
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
+
+class MovieList extends Component {
+
+  state = {
+    spacing: '32',
+  };
 
     componentDidMount() {
         this.props.dispatch({type: 'GET_MOVIES' });
@@ -17,20 +36,37 @@ class Home extends Component {
 
   render() {
     // renders all the movies to the DOM
+    const { classes } = this.props;
+    const { spacing } = this.state;
 
     return (
-      <div>
-          <section>
-            {this.props.reduxStore.movies.map(movie => 
-                <div className="display card" key={movie.id}>
-                    <img className="homeImg" alt={movie.title} src={movie.poster} height="300px" width="200px"/>
-                    <br></br>
+      <Grid container className={classes.root} spacing={32}>
+        <Grid item xs={12}>
+          <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
+            {this.props.reduxStore.movies.map(movie =>
+              <Grid className="display card" key={movie.id}>
+                <img className="homeImg" alt={movie.title} src={movie.poster} height="300px" width="200px"/>
+                <br></br>
                     <button className="button" 
-                            onClick={() => this.goToDetails(movie.id)}>{movie.title}</button>
-                </div>
+                             onClick={() => this.goToDetails(movie.id)}>{movie.title}</button>
+              </Grid>
             )}
-          </section>
-      </div>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      // <div>
+      //     <section>
+      //       {this.props.reduxStore.movies.map(movie => 
+      //           <div className="display card" key={movie.id}>
+      //               <img className="homeImg" alt={movie.title} src={movie.poster} height="300px" width="200px"/>
+      //               <br></br>
+      //               <button className="button" 
+      //                       onClick={() => this.goToDetails(movie.id)}>{movie.title}</button>
+      //           </div>
+      //       )}
+      //     </section>
+      // </div>
     );
   }
 }
@@ -39,4 +75,8 @@ const putReduxStateOnProps = (reduxStore) => ({
     reduxStore,
 });
 
-export default connect(putReduxStateOnProps)(Home);
+MovieList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default connect(putReduxStateOnProps)(withStyles(styles)(MovieList));
