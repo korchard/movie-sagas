b8de7c3c1193b0a5f1177249f253baa6cc69d8fc
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2020-12-20 11:29:17 -0600

    tried to work on multiple categories, but its a big rabbit hole - will come back to this

--- src/components/EditMovie/EditMovie.js
+++ src/components/EditMovie/EditMovie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+// import { Multiselect } from 'multiselect-react-dropdown';
 import './EditMovie.css';
 
 class EditMovie extends Component {
--- src/components/MovieForm/MovieForm.js
+++ src/components/MovieForm/MovieForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+// import { Multiselect } from 'multiselect-react-dropdown';
 import './MovieForm.css';
 
 class MovieForm extends Component {
